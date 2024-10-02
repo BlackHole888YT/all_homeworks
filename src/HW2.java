4e1182bf561a705ee9a78cdb27e021b41571df67
Iskander <blackhole888yt@gmail.com>
2024-10-02 17:39:28 +0600

    Метод permission теперь возвращаемый

--- src/HW2.java
+++ src/HW2.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
 public class HW2 {
-    public static void permission (int age, int temperature) { // 1 и 2 пункт
+    public static String permission (int age, int temperature) { // 1 и 2 пункт
         if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else if (age < 20 && temperature >= 0 && temperature <= 28) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else if (age >= 45 && temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else {
-            System.out.println("Оставайтесь дома");
+            return "Оставайтесь дома";
         }
     }
 
@@ -19,12 +19,12 @@ public class HW2 {
     }
 
     public static void main(String[] args) { // 3 пункт (вызвать 5 раз)
-        permission(10, -30);
-        permission(15, 10);
-        permission(20, 18);
-        permission(35, 35);
-        permission(45, 10);
+        System.out.println(permission(10, -30));
+        System.out.println(permission(15, 10));
+        System.out.println(permission(20, 18));
+        System.out.println(permission(35, 35));
+        System.out.println(permission(45, 10));
 
-        permission(generateRandomAge(), 10); // 5 пункт
+        System.out.println(permission(generateRandomAge(), 10)); // 5 пункт
     }
 }
